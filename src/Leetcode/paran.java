9b53d426e0c78d99e1e553caf7aa91a58ea528ab
Gokulnathsvr <gokulnathgokulsvr@gmail.com>
2024-07-26 15:04:43 +0530

    Leetcode

--- src/Leetcode/paran.java
+++ src/Leetcode/paran.java
@@ -1,6 +1,8 @@
+package Leetcode;
+
 import java.util.*;
 
-public class Main {
+public class paran {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
